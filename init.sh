93cc3e90da977b0daad413733803b7b89d7c3dab
lisaac <lisaac.cn@gmail.com>
2019-02-18 10:05:51 +0800

    bug fix

--- init.sh
+++ init.sh
@@ -5,10 +5,12 @@ touch /etc/ss-tproxy/gfwlist.txt
 touch /etc/ss-tproxy/chnroute.txt
 touch /etc/ss-tproxy/chnroute.set
 NEED_EXIT=0
-[ ! -f "$CONFIG_PATH"/ss-tproxy.conf ] && { cp /copy_config/ss-tproxy.conf "$CONFIG_PATH"; echo "[ERR] No ss-tproxy.conf, sample file copied, please config it."  1>&2; NEED_EXIT=1; }
-[ ! -f "$CONFIG_PATH"/v2ray.conf ] && { cp /copy_config/v2ray.conf "$CONFIG_PATH"; echo "[ERR] no v2ray.conf, sample file copied, please config it."  1>&2; NEED_EXIT=1; }
-[ ! -f "$CONFIG_PATH"/gfwlist.ext ] && { cp /copy_config/gfwlist.ext "$CONFIG_PATH"; }
-[ "$NEED_EXIT" = 1 ] && { exit 1; }
+[ ! -f "$CONFIG_PATH"/ss-tproxy.conf ] && { cp /sample_config/ss-tproxy.conf "$CONFIG_PATH"; echo "[ERR] No ss-tproxy.conf, sample file copied, please config it."  1>&2; NEED_EXIT=1; }
+[ ! -f "$CONFIG_PATH"/v2ray.conf ] && { cp /sample_config/v2ray.conf "$CONFIG_PATH"; echo "[ERR] no v2ray.conf, sample file copied, please config it."  1>&2; NEED_EXIT=1; }
+[ ! -f "$CONFIG_PATH"/gfwlist.ext ] && { cp /sample_config/gfwlist.ext "$CONFIG_PATH"; }
+if [ "$NEED_EXIT" = 1 ]; then
+  exit 1;
+fi
 source "$CONFIG_PATH"/ss-tproxy.conf
 if [ "$mode" = chnroute ]; then
   echo "`date +%Y-%m-%d\ %T` updating chnroute.."
@@ -28,10 +30,12 @@ echo "`date +%Y-%m-%d\ %T` flushing gfwlist.."
 /usr/local/bin/ss-tproxy flush-gfwlist
 echo "`date +%Y-%m-%d\ %T` flushing dnscache.."
 /usr/local/bin/ss-tproxy flush-dnscache
-killall crond
+kill -9 $(pidof crond) &>/dev/null
 grep -n '^[^#]*/init.sh' /etc/crontabs/root && crond
 echo "`date +%Y-%m-%d\ %T` staring tproxy-gateway.."
 /usr/local/bin/ss-tproxy restart && \
 echo -e "IPv4 gateway & dns server: \n`ip addr show eth0 |grep 'inet ' | awk '{print $2}' |sed 's/\/.*//g'`" && \
-echo -e "IPv6 dns server: \n`ip addr show eth0 |grep 'inet6 ' | awk '{print $2}' |sed 's/\/.*//g'`" && \
-tail -f /dev/null
+echo -e "IPv6 dns server: \n`ip addr show eth0 |grep 'inet6 ' | awk '{print $2}' |sed 's/\/.*//g'`"
+if [ "$1" = daemon ]; then
+  tail -f /dev/null
+fi
