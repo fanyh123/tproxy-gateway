1c40f40197a73a087378fe6c7e31d393f7b01338
lisaac <lisaac.cn@gmail.com>
2019-03-05 13:23:15 +0800

    加入自定义hosts

--- init.sh
+++ init.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 CONFIG_PATH='/etc/ss-tproxy'
-touch /etc/ss-tproxy/gfwlist.txt
-touch /etc/ss-tproxy/chnroute.txt
-touch /etc/ss-tproxy/chnroute.set
 NEED_EXIT=0
-[ ! -f "$CONFIG_PATH"/ss-tproxy.conf ] && { cp /sample_config/ss-tproxy.conf "$CONFIG_PATH"; echo "[ERR] No ss-tproxy.conf, sample file copied, please config it."  1>&2; NEED_EXIT=1; }
-[ ! -f "$CONFIG_PATH"/v2ray.conf ] && { cp /sample_config/v2ray.conf "$CONFIG_PATH"; echo "[ERR] no v2ray.conf, sample file copied, please config it."  1>&2; NEED_EXIT=1; }
+[ ! -f "$CONFIG_PATH"/ss-tproxy.conf ] && { cp /sample_config/ss-tproxy.conf "$CONFIG_PATH"; echo "[ERR] No ss-tproxy.conf, sample file copied, please configure it."  1>&2; NEED_EXIT=1; }
+[ ! -f "$CONFIG_PATH"/v2ray.conf ] && { cp /sample_config/v2ray.conf "$CONFIG_PATH"; echo "[ERR] no v2ray.conf, sample file copied, please configure it."  1>&2; NEED_EXIT=1; }
 [ ! -f "$CONFIG_PATH"/gfwlist.ext ] && { cp /sample_config/gfwlist.ext "$CONFIG_PATH"; }
 if [ "$NEED_EXIT" = 1 ]; then
   exit 1;
 fi
+
 source "$CONFIG_PATH"/ss-tproxy.conf
+[ ! -f "$file_gfwlist_txt"  ] && touch $file_gfwlist_txt
+[ ! -f "$file_chnroute_txt"  ] && touch $file_chnroute_txt
+[ ! -f "$file_chnroute_set"  ] && touch $file_chnroute_set
+[ ! -f "$dnsmasq_addn_hosts"  ] && touch $dnsmasq_addn_hosts
+
 if [ "$mode" = chnroute ]; then
   echo "`date +%Y-%m-%d\ %T` updating chnroute.."
   /usr/local/bin/ss-tproxy update-chnroute
